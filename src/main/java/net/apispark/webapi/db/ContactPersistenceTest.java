128a2cd62b6d91a1b22e99c9284e296b351b60b4
anqitong <none@none.com>
2015-10-27 14:44:40 +0100

    modif sur mon repo

---
+++ src/main/java/net/apispark/webapi/db/ContactPersistenceTest.java
@@ -0,0 +1,14 @@
+package net.apispark.webapi.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactPersistenceTest extends ContactPersistence {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
