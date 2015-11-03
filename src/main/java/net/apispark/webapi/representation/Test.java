1b6d6af6c91cdf582e9d08b1c909484c4bf7a428
anqitong <none@none.com>
2015-11-03 14:32:04 +0100

    add a test that succeeds

---
+++ src/main/java/net/apispark/webapi/representation/Test.java
@@ -0,0 +1,15 @@
+package net.apispark.webapi.representation;
+
+import static org.junit.Assert.*;
+
+import junit.framework.Assert;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		Contact c = new Contact("123", "firstname", "lastname", "avatar");
+		Assert.assertEquals(true, c.getId().equals("123"));
+	}
+
+}
